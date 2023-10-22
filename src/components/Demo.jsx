dca58703d9c35549800ff5131fe390badaf24c41
suhel <suhel7822@gmail.com>
2023-10-23 01:04:55 +0530

    added hero and demo comps

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,5 +1,50 @@
+import { useState, useEffect } from "react";
+import { AiOutlineEnter } from "react-icons/ai";
+
+import { linkIcon, loader, copy, tick } from "../assets/index";
+
 const Demo = () => {
-  return <div>demo</div>;
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+          <input
+            type="url"
+            placeholder="Enter a url"
+            value={article.url}
+            onChange={(e) => {
+              setArticle({ ...article, url: e.target.value });
+            }}
+            required
+            className="url_input peer"
+          />
+          <button type="submit" className="submit_btn peer-focus:text-gray-700">
+            <AiOutlineEnter />
+          </button>
+        </form>
+
+        {/* browsing history */}
+      </div>
+      {/* showing result */}
+    </section>
+  );
 };
 
 export default Demo;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,16 +3,26 @@ import { logo } from "../assets";
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-      <nav className="flex justify-between items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
         <img src={logo} alt="sumz logo" />
 
         <button
           type="butoon"
           onClick={() => window.open("https://github.com/Suhelkhan12")}
+          className="black_btn"
         >
           Github
         </button>
       </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-5</span>
+      </h1>
+      <h2 className="desc">
+        Simpligy your reading with Summizer, an article summarizer that
+        transforms lengthy articles into clear and concise summaries.
+      </h2>
     </header>
   );
 };
